b049d585a7ae797a984d385345947f9914b6402e
vasu <vasubabuparise8@gmail.com>
2022-11-05 18:32:26 +0530

    first commit

---
+++ src/main/java/com/pnc/demo/OuterToMockApplication.java
@@ -0,0 +1,13 @@
+package com.pnc.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OuterToMockApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OuterToMockApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pnc/demo/OuterToMockApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pnc.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OuterToMockApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
